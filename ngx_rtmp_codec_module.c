d79767aaa8bd216400158e71dd0509dc113b24f8
vnsducson29 <vnsducson@gmail.com>
2018-08-30 17:43:11 +0700

    update server name

--- ngx_rtmp_codec_module.c
+++ ngx_rtmp_codec_module.c
@@ -581,7 +581,7 @@ ngx_rtmp_codec_reconstruct_meta(ngx_rtmp_session_t *s)
 
         { NGX_RTMP_AMF_STRING,
           ngx_string("Server"),
-          "NGINX RTMP (github.com/arut/nginx-rtmp-module)", 0 },
+          "KCDN RTMP", 0 },
 
         { NGX_RTMP_AMF_NUMBER,
           ngx_string("width"),
